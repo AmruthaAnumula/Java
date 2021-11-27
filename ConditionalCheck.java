57ac125f1d9f7d546bda6cc0f98c3f6f28f3d3a9
Anumula Amrutha <93435292+AmruthaAnumula@users.noreply.github.com>
2021-11-27 10:23:51 +0000

    Update ConditionalCheck.java

--- ConditionalCheck.java
+++ ConditionalCheck.java
@@ -8,11 +8,11 @@ if(n%2!=0)
 {
 System.out.println("weird");
 }
-else if(n>2 && n>=5)
+else if(n>=2 && n<=5)
 {
 System.out.println("Not weird");
 }
-else if(n>6 && n>=20)
+else if(n>=6 && n<=20)
 {
 System.out.println("weird");
 }
