f7a47dc63ce3b0b50bfb85525e5cdb3bfb834039
AmruthaAnumula <ammuamrutha35423@gmail.com>
2021-12-28 01:34:34 -0500

    Basic Programs

---
+++ Day1/ConditionalCheck.java
@@ -0,0 +1,28 @@
+class ConditionalCheck
+{
+public static void main(String args[])
+{
+int n=24;
+
+if(n%2!=0)
+{
+System.out.println("weird");
+}
+else if(n>=2 && n<=5)
+{
+System.out.println("Not weird");
+}
+else if(n>=6 && n<=20)
+{
+System.out.println("weird");
+}
+else
+{
+System.out.println("Not weird");
+}
+}
+}
+
+OUTPUT-
+Not weird
+
